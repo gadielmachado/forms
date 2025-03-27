f3ca6bc17c52098740040e6a2ca1e781ef9595f1
gadielmachado <gadielmachado.bm@gmail.com>
2025-03-27 10:40:13 -0300

    fix: force npm usage and simplify build process

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/",
   server: {
     port: 3000,
     strictPort: false,
